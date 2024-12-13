8d14c9c00d913bd0ab345f19030a267628050e93
DMikaia <faeldan03@gmail.com>
2024-12-13 16:37:52 +0300

    Created a better input handler and added file clearing before saving it

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,19 +1,25 @@
 use app::{get_choice_from_input, init_file};
-use std::{collections::HashMap, env, fs::File};
+use std::{
+    collections::HashMap,
+    env,
+    fs::File,
+    io::{stdin, Stdin},
+};
 use task::{display_task, load_task, save_task, Task};
 
 fn main() -> Result<(), String> {
     let mut target_file: File = init_file(env::args());
     let tasks: HashMap<usize, Task> = load_task(&mut target_file);
+    let stdin: Stdin = stdin();
 
+    println!();
     loop {
-        println!();
-        match get_choice_from_input() {
+        match get_choice_from_input(&stdin)? {
             1 => display_task(&tasks),
             2 => println!("Choice = 2"),
             3 => println!("Choice = 3"),
             4 => {
-                println!("Shutting down...");
+                println!("Shutting down...\n");
                 save_task(&mut target_file, tasks)?;
 
                 break;
